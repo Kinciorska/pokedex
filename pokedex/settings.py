c2a5843f929fb047c28a68db009a865b15bc06a9
Kinciorska <kinga.kincso.kollo@gmail.com>
2023-05-31 12:36:18 +0200

    add postgreSQL as db

--- pokedex/settings.py
+++ pokedex/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'pokedex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pokedex',
+        'USER': 'pokedexuser',
+        'PASSWORD': 'kinga',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
