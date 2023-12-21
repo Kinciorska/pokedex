8574450c030a04f29821769a2fa076b8ac5b63b9
Kinciorska <kinga.kincso.kollo@gmail.com>
2023-12-21 11:24:26 +0100

    fix: added CORS handling

--- pokedex/settings.py
+++ pokedex/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'pokemons',
     'website',
     'pokemon_types',
@@ -66,7 +67,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'pokedex.urls'
 
