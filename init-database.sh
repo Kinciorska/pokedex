d74b71c012be1154e8481d9a72b0b398bb772b93
Kinciorska <kinga.kincso.kollo@gmail.com>
2024-09-19 10:28:23 +0200

    refactor - changed django user permission to allow db creation

--- init-database.sh
+++ init-database.sh
@@ -4,4 +4,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER pokedexuser WITH ENCRYPTED PASSWORD 'pokedexpassword';
     CREATE DATABASE pokedex ENCODING 'UTF8'  OWNER pokedexuser;
+    ALTER USER pokedexuser CREATEDB;
 EOSQL
